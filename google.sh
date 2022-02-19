49fa6ab06475e4704eab0e46a0eaa2328b48a1e3
matheu1995 <matheusosilva1995@gmail.com>
2022-02-19 18:23:07 -0300

    Fixed pipeline script

--- google.sh
+++ google.sh
@@ -3,3 +3,4 @@
 set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+chmod +x ./Tets.Automacao/driver/chromedriver
